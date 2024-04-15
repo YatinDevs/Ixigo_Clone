e6ef951c967de723935c25dfd5084df8ac82685a
YatinDevs <c.yatin727@gmail.com>
2024-04-15 23:15:01 +0530

    feat: Train Card

--- src/pages/Trains/TrainSearchPage/TrainsListing/TrainCard.jsx
+++ src/pages/Trains/TrainSearchPage/TrainsListing/TrainCard.jsx
@@ -59,8 +59,8 @@ function TrainCard({
   return (
     <>
       <ContentWrapper>
-        <div className="mx-2 md:mx-5">
-          <div className="w-full text-xs  md:text-lg flex justify-between items-center  h-[150px] md:h-[200px] bg-white shadow-md border">
+        <div className="mx-2 md:mx-5 shadow-md border">
+          <div className="w-full border-b text-xs  md:text-lg flex justify-between items-center  h-[120px] md:h-[160px] bg-white ">
             <div className=" w-[150px] md:w-[450px] flex flex-col gap-2 border-r px-2 md:px-4 ">
               <p className="text-orange-600 flex flex-col md:flex-row gap-2  md:font-semibold uppercase">
                 <span className="">{trainNumber}</span>
@@ -127,6 +127,27 @@ function TrainCard({
               </div>
             </div>
           </div>
+          <div className="flex">
+            <div className=" grid grid-cols-4 md:grid-cols-5 lg:grid-cols-7  gap-2  text-xs md:text-lg border-b shadow-sm p-2">
+              {coaches?.map((coach) => (
+                <>
+                  <div className="h-14 md:h-20 pt-2 px-2 flex flex-col gap-2 rounded-md border bg-green-50">
+                    <div className="flex gap-2 justify-center items-center">
+                      <p>{coach.coachType}</p>
+                      <div className="h-1 w-1  bg-black rounded flex justify-center self-center items-center"></div>
+                      <p className="">
+                        <span>&#x20B9;</span>
+                        {getFare(coach.coachType, fare)}
+                      </p>
+                    </div>
+                    <p className="text-center text-green-700">
+                      AVL {coach.numberOfSeats}
+                    </p>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
           {showDetails && <TrainAvailabilityCard />}
         </div>
       </ContentWrapper>
@@ -135,3 +156,25 @@ function TrainCard({
 }
 
 export default TrainCard;
+
+function getFare(type, baseFare) {
+  switch (type) {
+    case "CC":
+      return Math.round(baseFare * 2);
+    case "2S":
+      return Math.round(baseFare * 0.75);
+    case "SL":
+      return Math.round(baseFare);
+    case "1A":
+      return Math.round(baseFare * 6.25);
+    case "2A":
+      return Math.round(baseFare * 3.5);
+    case "3A":
+      return Math.round(baseFare * 2.75);
+    case "3E":
+      return Math.round(baseFare * 1.75);
+    case "EA":
+      return Math.round(baseFare * 5);
+  }
+  return baseFare;
+}
