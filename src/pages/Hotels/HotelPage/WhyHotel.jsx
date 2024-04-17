f01cca010fda8b16f35424f7f8abe4d115c53bad
YatinDevs <c.yatin727@gmail.com>
2024-04-17 20:53:09 +0530

    feat:hotel Homepage

--- src/pages/Hotels/HotelPage/WhyHotel.jsx
+++ src/pages/Hotels/HotelPage/WhyHotel.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import CarouselWhy from "../components/Carouselwhy";
 import { HOTEL_OFFERS_CAROUSEL } from "../hotel";
+import CarouselWhy from "../components/Carouselwhy";
 function WhyHotel() {
   return (
     <div>
-      {" "}
       <CarouselWhy data={HOTEL_OFFERS_CAROUSEL} />
     </div>
   );
